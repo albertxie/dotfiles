64d3a81e5bb1b67f1afc5499042e2add1e85da4d
Albert Xie <albert.xie27@gmail.com>
2023-12-24 23:02:47 -0500

    LSP diagnostics on hover

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -47,6 +47,35 @@ vim.api.nvim_set_keymap("n", "<M-LeftMouse>", ":GBrowse <CR>", { silent = true }
 vim.api.nvim_set_keymap("n", "<LEADER>d", ":Telescope lsp_definitions <CR>", {})
 vim.api.nvim_set_keymap("n", "<LEADER>r", ":Telescope lsp_references<CR>", {})
 
+-- Function to check if a floating dialog exists and if not
+-- then check for diagnostics under the cursor
+function OpenDiagnosticIfNoFloat()
+  for _, winid in pairs(vim.api.nvim_tabpage_list_wins(0)) do
+    if vim.api.nvim_win_get_config(winid).zindex then
+      return
+    end
+  end
+  -- THIS IS FOR BUILTIN LSP
+  vim.diagnostic.open_float(0, {
+    scope = "cursor",
+    focusable = false,
+    close_events = {
+      "CursorMoved",
+      "CursorMovedI",
+      "BufHidden",
+      "InsertCharPre",
+      "WinLeave",
+    },
+  })
+end
+-- Show diagnostics under the cursor when holding position
+vim.api.nvim_create_augroup("lsp_diagnostics_hold", { clear = true })
+vim.api.nvim_create_autocmd({ "CursorHold" }, {
+  pattern = "*",
+  command = "lua OpenDiagnosticIfNoFloat()",
+  group = "lsp_diagnostics_hold",
+})
+
 -- Colorscheme
 vim.cmd("colorscheme tokyonight")
 vim.cmd("syntax on")
